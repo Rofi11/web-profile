e05859b3e4b5687253edac42c84f68783d69892e
Muhammad Rofi <98885565+Rofi11@users.noreply.github.com>
2024-02-23 14:27:38 +0700

    feat/profile-home : main page and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-import Navbar from "@/components/Navbar/Navbar";
+import Navbar from "@/components/Navbar";
+import MainPage from "@/components/MainPage";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <main>
+      {/* navbar */}
       <Navbar />
+
+      {/* main page */}
+      <MainPage />
+
+      {/* Footer */}
+      <Footer />
     </main>
   );
 }
